051d84aa2bc1dd607334f34b258fe65faf94bdf0
ibrohim-isroilov <emirisroilov@gmail.com>
2026-01-15 19:20:55 +0300

    lesson-2-poem
    poem

---
+++ lesson-2/lesson-2-poem.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 15 18:36:51 2026
+
+@author: Emir
+"""
+import math
+emir=math.inf
+Emir="The Uvinerse"
+Poet="Umar Hayyom"
+Sufi= "Jalollidn Muhammad Rumi"
+print("Odam ko'rdim loyqa suvni tindirgan") #This code has been written in Python using double quotes.
+print("Odam ko'rdim o'z kosasin sindirgan") #This code has been written in Python using double quotes.
+print("Odam ko'rdim  yaxshlikni ko'zlagan") #This code has been written in Python using double quotes.
+print("Odam ko'rdim turqi uni muzlagan")    #This code has been written in Python using double quotes.
+#This is second type of  quote
+print("")
+print('Odam ko\'rdim loyqa suvni tindirgan')  #This code has been written in Python using single quotes.
+print('Odam ko\'rdim o\'z kosasin sindirgan') #This code has been written in Python using single quotes.
+print('Odam ko\'rdim yaxshilikni ko\'zlagan') #This code has been writtem in Python using single quotes.
+print('Odam ko\'rdim turqi uni muzlagan')     #This code has been written in Python using single quotes.
+#This codes has been written in Python using Triple quotes.
+print("")
+print("""Odam  ko'rdim  loyqa suvni tindirgan
+Odam ko'rdim o'z kosasin sindirgan
+Odam ko'rdin yaxshilikni ko'zlagan
+Odam ko'rdim  turqi uni muzlagan""")
+#This code has been written in Pyhtonusing single-triple quote
+print("")
+print('''Odam ko\''rdim loyqa suvni tindirgan
+Odam ko\'rdim o\'z kosasin sindirgan
+Odam ko\'rdim yaxshilikni ko'zlagan
+Odam ko\'rdim turqi uni muzlagan ''')#This code has been writen in Pyhton using single-triple quotes
+print(emir)
