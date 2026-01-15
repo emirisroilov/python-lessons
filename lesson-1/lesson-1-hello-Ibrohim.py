679e4f91f9111891160e245515f8e8708b7d5895
ibrohim-isroilov <emirisroilov@gmail.com>
2026-01-15 17:51:38 +0300

    the-first-push
    lesson-1-day-1

---
+++ lesson-1/lesson-1-hello-Ibrohim.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 15 17:34:38 2026
+
+@author: Ibrohim
+"""
+
+emir="Ibrohim" #Today, I’m starting to code again.”
+print("Hello "+ emir)
+print("Long live Bukhara — eternal and magnificent.") #Long live Bukhara — eternal and magnificent.
---
+++ lesson-1/lesson-1-hello-emir.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 15 17:27:47 2026
+
+@author: Emir
+"""
+Emir="The World"
+print("Hello Emir")
+print("Hello " +Emir)
+print("This is my first piece of code in the Python programming language.")
