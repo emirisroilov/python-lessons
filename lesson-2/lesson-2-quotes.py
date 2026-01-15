748206427486fc6dc01d78b3022837eb799cc836
ibrohim-isroilov <emirisroilov@gmail.com>
2026-01-15 18:27:53 +0300

    lesson-2
    Description of Quotes

---
+++ lesson-2/lesson-2-quotes.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 15 17:54:34 2026
+
+@author: Ibrohim
+"""
+
+print("HEllo Emir") # This my code printed in Python with double quote
+print("Hello Emir. Emir tahs is my nickname") # This is my code printed in Pythoon with double  quote
+print("This code printed with  double quote")
+print('This code printed with single quote')
